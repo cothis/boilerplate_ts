af697f051c5f95141deaa2ddf4ec054499b6ac15
cothis <minho1030@naver.com>
2021-07-26 00:34:53 +0900

    add : server boilerplate

---
+++ server/src/app.ts
@@ -0,0 +1,32 @@
+import 'dotenv-defaults/config';
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import session from 'express-session';
+import { dirname } from 'path/posix';
+const NedbStore = require('nedb-session-store')(session);
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.use(
+  session({
+    secret: 'sample',
+    store: new NedbStore({
+      filename: 'sessionStore.db',
+    }),
+  })
+);
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../pbulic/index.html'));
+});
+
+export default app;
